4b483342861ab98ebc48823c715a07aa1b5c79c6
sanjeewa.nibm@gmail.com <sanjeewa.nibm@gmail.com>
2019-03-12 08:12:46 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+// import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Invitation App';
+
+  httpdata;
+ constructor() { }
+
+
+
+
+
+   // tslint:disable-next-line:use-life-cycle-interface
+   ngOnInit() {
+
+
+    //  this.http.get('http://jsonplaceholder.typicode.com/users')
+    // //  .subscribe((data) => console.log(data.json()));
+    //     //  .pipe(map(res => console.log(res.json())))
+    //     .subscribe((data) => this.displaydata(data));
+
+    // this.http.get('http://jsonplaceholder.typicode.com/users')
+    //   .pipe(map(response => response.json()))
+    //   .subscribe((data) => this.displaydata(data));
+   }
+
+//    displaydata(data) {
+//      this.httpdata = data;
+//      console.log(data);
+//     }
+//   onClickSubmit(data) {
+//     alert('Entered Email id : ' + data.emailid);
+//  }
+}
